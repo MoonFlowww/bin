42c7c27ecaa16340e869fe88057ce2824c76bb51
MoonFlowww <161968556+MoonFlowww@users.noreply.github.com>
2024-09-29 14:43:45 +0200

    Create Cout_Array.cpp

---
+++ Cout_Array.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <chrono>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+
+std::ostringstream oss;
+
+std::string marge(const std::string& str, int totalWidth) {
+    if (str.size() >= static_cast<size_t>(totalWidth)) {
+        return str;
+    }
+    int padding = (totalWidth - static_cast<int>(str.size())) / 2;
+    int extra = (totalWidth - static_cast<int>(str.size())) % 2;
+    return std::string(padding, ' ') + str + std::string(padding + extra, ' ');
+}
+
+void array(std::vector<std::string>& s, bool title = false, bool color = false, bool addSpace = false) {
+    std::vector<int> widths;
+    int heights = s.size();
+
+    for (int i = 0; i < heights; ++i) widths.push_back(s[i].size());
+    int total_w = *std::max_element(widths.begin(), widths.end()) + 8 + 2; // 2 '|' + 8 ' '
+    std::string Hbar = "+" + std::string(total_w - 2, '-') + "+";
+    std::string Vbar = "|";
+    if (color) {
+        Hbar.insert(0, "\033[1m\033[36m");
+        Hbar += "\033[0m";
+        Vbar.insert(0, "\033[1m\033[36m");
+        Vbar += "\033[0m";
+    }
+    if (addSpace) {
+        s.insert(s.begin() + 1, "  ");
+        s.push_back("  ");
+        heights += 2;
+    }
+
+    if (title) {
+        std::string w = marge(s[0], total_w - 2);
+        oss << Hbar << "\n";
+        oss << Vbar
+            << "\033[1m\033[37m"
+            << w
+            << "\033[0m"
+            << Vbar
+            << "\n";
+    }
+
+    oss << Hbar << "\n";
+    for (int i = title ? 1 : 0; i < heights; ++i) {
+        std::string w = marge(s[i], total_w - 2);
+        oss << Vbar
+            << "\033[1m\033[37m"
+            << w
+            << "\033[0m"
+            << Vbar
+            << "\n";
+    }
+    oss << Hbar << "\n";
+}
+
+int main() {
+    std::vector<std::string> s = { "Title", "Hello World", "C++", "LeetCode", "HomeMade" };
+    array(s, true, true);
+    std::cout << oss.str(); // cout everything 
+
+    return 0;
+}
